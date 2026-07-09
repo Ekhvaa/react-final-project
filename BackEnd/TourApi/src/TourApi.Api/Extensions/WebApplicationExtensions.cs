ef99e1ffb4e722dff378a26762f0e7ecfb7d985e
ladothegreat <ladomaisuradze2@gmail.com>
2026-07-09 21:49:27 +0400

    Add swagger for deployment

--- BackEnd/TourApi/src/TourApi.Api/Extensions/WebApplicationExtensions.cs
+++ BackEnd/TourApi/src/TourApi.Api/Extensions/WebApplicationExtensions.cs
@@ -13,12 +13,9 @@ public static class WebApplicationExtensions
 {
     public static WebApplication UseSwaggerDocumentation(this WebApplication app)
     {
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
+        app.UseSwagger();
+        app.UseSwaggerUI();
+      
         return app;
     }
 
