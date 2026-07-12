3668f941ecfceece25c23e28122be7c56e62140e
Ekhvaa <ekhvaianika@gmail.com>
2026-07-12 14:25:16 +0400

    Added bookmark functionality

--- FrontEnd/src/components/TourCard.jsx
+++ FrontEnd/src/components/TourCard.jsx
@@ -2,14 +2,28 @@ import React, { useEffect } from 'react'
 import photo from '../assets/TourCardPhoto.jpg'
 import locationIcon from '../assets/LocationIcon.svg'
 import starRating from '../assets/StarRating.svg'
+import BookmarkedIcon from '../assets/bookmarkedIcon.svg?react'
+import NotBookmarkedIcon from '../assets/notBookmarkedIcon.svg?react'
 import { Link } from 'react-router-dom'
+import { useTourBookmark } from '../hooks/useTourBookmark'
 
 
 const TourCard = ( { id, title, price, city, country, imageUrl, description } ) => {
+  const { bookmarkedTours, toggleBookmark, checkIsBookmarked} = useTourBookmark();
+
+  const handleBookmarkClick = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    toggleBookmark(title);
+  }
+
   return (
     <Link to={`/tours/${id}`} className='max-w-70 overflow-hidden cursor-pointer flex flex-col gap-2 rounded-3xl border border-gray-100 shadow-sm hover:shadow-xl transition-shadow duration-300'>
-      <div>
+      <div className='relative'>
         <img src={`https://tourist-platform-api-f9g6c9azezbvehf0.italynorth-01.azurewebsites.net${imageUrl}`} alt="TourPhoto" />
+        <div className='absolute top-4 right-4 rounded-full p-3 bg-black/30 cursor-pointer' onClick={(e) => handleBookmarkClick(e)}>
+          {checkIsBookmarked(title) ? <BookmarkedIcon /> : <NotBookmarkedIcon />}
+        </div>
       </div>
       <div className='px-4 pb-3 flex flex-col gap-2'>
         <div className='flex gap-2'>
@@ -27,7 +41,6 @@ const TourCard = ( { id, title, price, city, country, imageUrl, description } ) 
         <span className='font-bold text-2xl'>${price}</span>
       </div>
     </Link>
-
   )
 }
 
--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { TourBookmarkProvider } from './contexts/TourBookmarkContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <TourBookmarkProvider>
+        <App />
+      </TourBookmarkProvider>
     </BrowserRouter>
   </StrictMode>,
 )
